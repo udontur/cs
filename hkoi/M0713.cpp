fda64f0e5a42c83fa6fd34418d8aa1b884e4db1a
Hadrian <118540129+udontur@users.noreply.github.com>
2024-01-05 18:32:46 +0800

    Update M0713.cpp

--- hkoi/M0713.cpp
+++ hkoi/M0713.cpp
@@ -1,1 +1,14 @@
-v
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    long long n, a[5000001], ans=0;
+    cin>>n;
+    for(int i =0; i<n; i++) cin>>a[i];
+    sort(a, a+n);
+    if(!(n%2)){
+        ans=a[n/2]+a[n/2+1];
+        ans=ans/2;
+        cout<<ans;
+    }else cout<<a[n/2];
+    return 0;
+}
