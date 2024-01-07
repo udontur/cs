15396c7db2576f6165f139186318bc595e3a1ed1
Hadrian <118540129+udontur@users.noreply.github.com>
2024-01-07 12:50:06 +0800

    Create M17A1.cpp

---
+++ HKOI/M17A1.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    if(n==2000) cout << "14 9 8";
+    else if(n==2001) cout << "13 11 10";
+    else if(n==2002) cout << "17 13 8";
+    else if(n==2003) cout << "18 12 12";
+    else if(n==2004) cout << "19 15 13";
+    else if(n==2005) cout << "25 17 12";
+    else if(n==2006) cout << "40 22 13";
+    else if(n==2007) cout << "41 19 8";
+    else if(n==2008) cout << "30 29 11";
+    else if(n==2009) cout << "36 28 13";
+    else if(n==2010) cout << "28 20 16";
+    else if(n==2011) cout << "32 25 23";
+    else if(n==2012) cout << "37 20 17";
+    else if(n==2013) cout << "31 31 26";
+    else if(n==2014) cout << "36 32 8";
+    else if(n==2015) cout << "42 39 9";
+    else if(n==2016) cout << "55 21 13";
+    else if(n==2017) cout << "53 35 26";
+    return 0;
+}
