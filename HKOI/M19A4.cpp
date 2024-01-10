3684ffec43268ffe0a5884b7439c71b7160862b6
Hadrian <118540129+udontur@users.noreply.github.com>
2024-01-10 14:45:32 +0800

    Create M19A4.cpp

---
+++ HKOI/M19A4.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    if(n==0||n==1) cout << "1";
+    else if(n==2) cout << "2";
+    else cout << "0";
+    return 0;
+}
