dc7628248b69f60d9a8e5433819cc9aa8f128058
Hadrian <118540129+udontur@users.noreply.github.com>
2024-01-10 14:40:59 +0800

    Create M19A2.cpp

---
+++ HKOI/M19A2.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int h, w;
+    cin >> h >> w;
+    for(int i =0; i<h; i++) cout << "D";
+    for(int i =0; i<=w; i++) cout << "R";
+    cout << "U";
+    return 0;
+}
