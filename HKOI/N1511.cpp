c6dcef9abef7c22febbfe86be361f27eb18246d0
Hadrian <118540129+udontur@users.noreply.github.com>
2024-01-13 20:52:22 +0800

    Update N1511.cpp

--- HKOI/N1511.cpp
+++ HKOI/N1511.cpp
@@ -1,33 +1,33 @@
 #include <bits/stdc++.h>
+#define int long long
 using namespace std;
-typedef long long ll;
-ll t, n, x[100001], y[100001], e[100001], brk=0;
-unordered_map <ll, ll> a;
-ll fnd(ll u) {
+int t, n, x[100001], y[100001], e[100001], bye=0;
+unordered_map <int, int> a;
+int fnd(int u) {
     if (!a.count(u)) a[u] = u;
     return a[u]==u?u:a[u]=fnd(a[u]);
 }
-void un(ll u, ll v) {a[fnd(u)]=fnd(v);}
-int main() {
+void un(int u, int v) {a[fnd(u)]=fnd(v);}
+int32_t main() {
     cin >> t;
     while(t--){
-        brk=0;
+        bye=0;
         cin >> n;
         a.clear();
-        for(ll i =0; i<n; i++) cin >> x[i] >> y[i] >> e[i];
-        for(ll i =0; i<n; i++){
+        for(int i =0; i<n; i++) cin >> x[i] >> y[i] >> e[i];
+        for(int i =0; i<n; i++){
             if(e[i]) un(x[i], y[i]);
         }
-        for(ll i=0; i<n; i++){
+        for(int i=0; i<n; i++){
             if(!e[i]){
                 if(fnd(x[i])==fnd(y[i])){
-                    cout << "NO" << endl;
-                    brk=1;
+                    cout << "NO\n";
+                    bye=1;
                     break;
                 }
             }
         }
-        if(!brk) cout << "YES" << endl;
+        if(!bye) cout << "YES\n";
     }
     return 0;
 }
