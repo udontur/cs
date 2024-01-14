112926be3e7b55cd86193a9b0fc1d4d9aa3c632c
Hadrian <118540129+udontur@users.noreply.github.com>
2024-01-14 15:19:26 +0800

    Create NP1711.cpp

---
+++ HKOI/NP1711.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    freopen("math.in", "r", stdin);
+    freopen("math.out", "w", stdout);
+    long long a, b;
+    cin>>a>>b;
+    cout<<a*b-a-b;
+    return 0;
+}
