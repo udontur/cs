6fba20435205e33e20757a83fc1bad425fc0e402
Hadrian <118540129+udontur@users.noreply.github.com>
2024-01-19 17:54:34 +0800

    Update lcs.cpp

--- DP/lcs.cpp
+++ DP/lcs.cpp
@@ -3,35 +3,30 @@
 #include <algorithm>
 using namespace std;
 
-string lcs(string n, string s){
-    int dp[1001][1001]{0}, maxv=-1, x, y;
-    int nl=s.length(), sl=s.length();
-    string ans="";
-    bool val=0;
-    for(int i =1; i<=nl; i++){
-        for(int j =1; j<=sl; j++){
-            if(s[j-1]==n[i-1]) {
+string lcs(string a, string b){
+    int dp[1001][1001]{0}, an=a.length(), bn=b.length();
+    for(int i =1; i<=an; i++){
+        for(int j =1; j<=bn; j++){
+            if(a[i-1]==b[j-1]){
                 dp[i][j]=dp[i-1][j-1]+1;
-                maxv=max(dp[i][j], maxv);
             }
         }
     }
-    for(int i =1; i<=nl; i++){
-        for(int j =1; j<=sl; j++){
-            if(dp[i][j]==maxv){
-                x=i;
-                y=j;
-                val=1;
-                break;
+    int mx=-1e9, mxi, mxj;
+    for(int i =1; i<=an; i++){
+        for(int j =1; j<=bn; j++){
+            if(mx<dp[i][j]){
+                mx=dp[i][j];
+                mxi=i;
+                mxj=j;
             }
         }
-        if(val) break;
     }
-    while(dp[x][y]!=0){
-        ans+=n[x-1];
-        x--;
-        y--;
+    string s="";
+    while(dp[mxi][mxj]!=0){
+        s+=a[mxi-1];
+        mxi--, mxj--;
     }
-    reverse(ans.begin(), ans.end());
-    return ans;
+    reverse(s.begin(), s.end());
+    return s;
 }
