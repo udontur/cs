7cd6adc397efc01f871e287905ef28d8c3077751
Hadrian <118540129+udontur@users.noreply.github.com>
2024-01-19 17:53:27 +0800

    Update 01053.cpp

--- HKOI/01053.cpp
+++ HKOI/01053.cpp
@@ -1,35 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    int dp[1001][1001]{0}, maxv=-1, x, y;
-    string n, s, ans="";
-    bool val=false;
-    cin >> n >> s;
-    for(int i =1; i<=n.length(); i++){
-        for(int j =1; j<=s.length(); j++){
-            if(s[j-1]==n[i-1]) {
+    string a, b;
+    cin>>a>>b;
+    int dp[1001][1001]{0}, an=a.length(), bn=b.length();
+    for(int i =1; i<=an; i++){
+        for(int j =1; j<=bn; j++){
+            if(a[i-1]==b[j-1]){
                 dp[i][j]=dp[i-1][j-1]+1;
-                maxv=max(dp[i][j], maxv);
             }
         }
     }
-    for(int i =1; i<=n.length(); i++){
-        for(int j =1; j<=s.length(); j++){
-            if(dp[i][j]==maxv){
-                x=i;
-                y=j;
-                val=true;
-                break;
+    int mx=-1e9, mxi, mxj;
+    for(int i =1; i<=an; i++){
+        for(int j =1; j<=bn; j++){
+            if(mx<dp[i][j]){
+                mx=dp[i][j];
+                mxi=i;
+                mxj=j;
             }
         }
-        if(val) break;
     }
-    while(dp[x][y]!=0){
-        ans+=n[x-1];
-        x--;
-        y--;
+    string s="";
+    while(dp[mxi][mxj]!=0){
+        s+=a[mxi-1];
+        mxi--, mxj--;
     }
-    reverse(ans.begin(), ans.end());
-    cout << ans << endl;
+    reverse(s.begin(), s.end());
+    cout<<s;
     return 0;
 }
