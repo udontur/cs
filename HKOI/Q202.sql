c6018c4e8263e9671e2b21093370a491e75e7801
Hadrian <118540129+udontur@users.noreply.github.com>
2024-01-24 21:10:57 +0800

    Create Q202.sql

---
+++ HKOI/Q202.sql
@@ -0,0 +1,4 @@
+select * from book where isbn=9781974715466;
+select title from book where genre="Technology";
+select title, num_pages from book where author="Donald Knuth";
+select distinct author from book
