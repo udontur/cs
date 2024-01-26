3aba8f4e1c80b27a61ae55ab88edd1699e88716a
Hadrian <118540129+udontur@users.noreply.github.com>
2024-01-26 17:10:39 +0800

    Update D201.cpp

--- HKOI/D201.cpp
+++ HKOI/D201.cpp
@@ -1,12 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    int n, a=0, b=1;
-    cin >> n;
-    for(int i =1; i<=n; i++){
-        int c=a+b;
-        a=b;
-        b=c;
-    }
-    cout << a;
+    int n;
+    cin>>n;
+    int dp[41];
+    dp[0]=0, dp[1]=1;
+    for(int i =2; i<=n; i++) dp[i]=dp[i-1]+dp[i-2];
+    cout<<dp[n];
 }
