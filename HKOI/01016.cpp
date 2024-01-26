54536ab97f304f4b6a6a338c29336181b65b3b31
Hadrian <118540129+udontur@users.noreply.github.com>
2024-01-26 10:44:31 +0800

    Update 01016.cpp

--- HKOI/01016.cpp
+++ HKOI/01016.cpp
@@ -1,22 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main () {
-    int n, v[100001], dp1[100001], dp2[100001], maxs=0, mins=0, sum=0;
+int main(){
+    int n, a[100001], sum=0;
     cin >> n;
     for(int i =1; i<=n; i++){
-        cin >> v[i];
-        sum+=v[i];
+        cin>>a[i];
+        sum+=a[i];
     }
-    dp1[0]=0;
-    for(int i =1; i<=n; i++){
-        dp1[i]=max(dp1[i-1],0)+v[i];
-        maxs=max(maxs, dp1[i]);
-    }
-    dp2[0]=0;
-    for(int i =1; i<=n; i++){
-        dp2[i]=min(dp2[i-1],0)+v[i];
-        mins=min(mins, dp2[i]);
-    }
-    cout << max(maxs, sum-mins) << endl;
+    int dp1[100001], dp2[100001];
+    dp1[0]=0, dp2[0]=0;
+    for(int i =1; i<=n; i++)
+        dp1[i]=max(dp1[i-1]+a[i], a[i]);
+    int mx1=-(1<<31);
+    for(int i =1; i<=n; i++)
+        mx1=max(dp1[i], mx1);
+    for(int i =1; i<=n; i++)
+        dp2[i]=min(dp2[i-1]+a[i], a[i]);
+    int mn2=(1<<31)-1;
+    for(int i =1; i<=n; i++)
+        mn2=min(dp2[i], mn2);
+    cout<<max(mx1, sum-mn2);
     return 0;
 }
