2d3b591bf59887fb86755eba3a93d56e27d20c7b
Hadrian <118540129+udontur@users.noreply.github.com>
2024-04-09 16:19:14 +0800

    Update dfs.cpp

--- Graph/dfs.cpp
+++ Graph/dfs.cpp
@@ -2,6 +2,6 @@ void dfs(vector<int> adj[], int v, vector<int>& vis){
     vis[v]=1;
     for(int u: adj[v])
         if(!vis[u])
-            dfs(u, adj, vis);
+            dfs(adj, u, vis);
     return;
 }
