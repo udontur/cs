48d16b33068464eafd4b672c34d9236396f0648e
Hadrian <118540129+udontur@users.noreply.github.com>
2024-07-10 11:52:58 +0800

    Update M1701.cpp

--- Solutions/HKOI/M1701.cpp
+++ Solutions/HKOI/M1701.cpp
@@ -3,8 +3,7 @@ using namespace std;
 int main(){
     long long a, b, c, d;
     cin>>a>>b>>c>>d;
-    long long o=b+c;
-    o+=2*d;
+    long long o=(b+c)+(2*d);
     if(a>2*d) o+=((a-2*d)/2+a%2);
     cout<<o;
     return 0;
