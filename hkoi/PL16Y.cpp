9b2c45e0f7c97dc00de94e14a4c6748c3d7103f5
Hadrian Lau <118540129+udontur@users.noreply.github.com>
2024-09-12 17:27:57 +0800

    Create PL16Y.cpp

---
+++ hkoi/PL16Y.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int d, n;
+    cin>>d>>n;
+    cout<<fixed<<setprecision(6);
+    cout<<(acos(-1.0)*(d/2.0)*(d/2.0))/n;
+    return 0;
+}
