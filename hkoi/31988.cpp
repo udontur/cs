5d06af9da31554fe3036ad3e036ad9b8084df9a6
Hadrian Lau <118540129+udontur@users.noreply.github.com>
2024-10-20 15:42:16 +0800

    Update 31988.cpp

--- hkoi/31988.cpp
+++ hkoi/31988.cpp
@@ -3,30 +3,16 @@ using namespace std;
 int main() {
     string s;
     while(getline(cin, s)){
-        string temp, ans, c;
-        bool home=0, end=0;
-        int len=s.length(), itr=0;
-        for(int i =0; i<len; i++){
-            if(s[i]=='['){
-                home=1;
-                end=0;
-                itr=0;
-            }else if(s[i]==']'){
-                home=0;
-                end=1;
-                itr=ans.length();
-            }else{
-                if(home||end){
-                    c=s[i];
-                    ans.insert(itr, c);
-                    itr++;
-                }else{
-                    ans+=s[i];
-                    itr=i;
-                }
-            }
+        int n=s.size();
+        list<char> a;
+        auto it=a.end();
+        for(int i =0; i<n; i++){
+            if(s[i]=='[') it=a.begin();
+            else if(s[i]==']') it=a.end();
+            else a.insert(it, s[i]);
         }
-        cout << ans << "\n";
+        for(auto i: a) cout<<i;
+        cout<<"\n";
     }
     return 0;
 }
